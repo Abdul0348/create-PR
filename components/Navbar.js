e15ed3198786fa07575481f76dc7a8d14697ea11
Abdul Rahman <arsalar286@gmail.com>
2024-01-23 12:09:32 +0500

    Navbar added

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,26 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <div className="navbar">
+        <div>
+          <li>
+            <ul>
+              {" "}
+              <a href="/">Home</a>
+            </ul>
+          </li>
+          <li>
+            <ul>
+              {" "}
+              <a href="/contact">Contact Us</a>
+            </ul>
+          </li>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
